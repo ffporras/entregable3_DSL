e58f86b90074ccaf28efafc2caf13e968ad6599f
ffporras <fporras@correo.um.edu.uy>
2024-10-27 10:47:45 -0300

    Traductor USQL a SQL listo

--- src/traductorUSQLaSQL.py
+++ src/traductorUSQLaSQL.py
@@ -1,13 +1,13 @@
 import ply.lex as lex
 import ply.yacc as yacc
-
+import sqlparse
 
 palabras_clave = (
     'TRAEME', 'TODO', 'DE_LA_TABLA', 'DONDE', 'AGRUPANDO_POR', 'MEZCLANDO', 'EN', 'LOS_DISTINTOS', 'CONTANDO', 'METE_EN',
-    'LOS_VALORES', 'ACTUALIZA', 'SETEA', 'BORRA_DE_LA', 'ORDENA_POR', 'COMO_MUCHO', 'WHERE_DEL_GROUP_BY', 'EXISTE', 'EN_ESTO',
+    'LOS_VALORES', 'ACTUALIZA', 'SETEA', 'BORRA_DE_LA_TABLA', 'ORDENA_POR', 'COMO_MUCHO', 'WHERE_DEL_GROUP_BY', 'EXISTE', 'EN_ESTO',
     'ENTRE', 'PARECIDO_A', 'ES_NULO', 'CAMBIA_LA_TABLA', 'AGREGA_LA_COLUMNA', 'ELIMINA_LA_COLUMNA', 'CREA_LA_TABLA', 
     'TIRA_LA_TABLA', 'POR_DEFECTO', 'UNICO', 'CLAVE_PRIMA', 'CLAVE_REFERENTE', 'NO_NULO', 'TRANSFORMA_A',
-    'Y', 'O', 'COUNT', 'VARCHAR', #Estos son agregados de la lista original
+    'Y', 'O', 'COUNT', 'VARCHAR' #Estos son agregados de la lista original
 )
 
 operadores_alfanumericos = (
@@ -15,7 +15,7 @@ operadores_alfanumericos = (
 )
 
 operadores_aritemticos = (
-    'SUMA', 'RESTA', 'MULTIPLICACION', 'DIVISION', 'IGUAL', 'MAYOR_QUE', 'MENOR_QUE', 'MAYOR_IGUAL', 'MENOR_IGUAL'
+    'SUMA', 'RESTA', 'DIVISION', 'IGUAL', 'MAYOR_QUE', 'MENOR_QUE', 'MAYOR_IGUAL', 'MENOR_IGUAL'
 )
 
 simbolos = (
@@ -36,7 +36,7 @@ t_METE_EN = r'METE\s+EN'
 t_LOS_VALORES = r'LOS\s+VALORES'
 t_ACTUALIZA = r'ACTUALIZA'
 t_SETEA = r'SETEA'
-t_BORRA_DE_LA = r'BORRA\s+DE\s+LA'
+t_BORRA_DE_LA_TABLA = r'BORRA\s+DE\s+LA\s+TABLA'
 t_ORDENA_POR = r'ORDENA\s+POR'
 t_COMO_MUCHO = r'COMO\s+MUCHO'
 t_EXISTE = r'EXISTE'
@@ -60,21 +60,19 @@ t_O = r'O'
 t_COUNT = r'COUNT'
 t_VARCHAR = r'VARCHAR'
 
-#t_NOMBRE = r'[a-zA-Z_][a-zA-Z0-9_]*'
 def t_NOMBRE(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     if t.value.upper() in palabras_clave:
-        print(f"Token encontrado: {t.value} como {t.value.upper()}")  # Imprimir el token encontrado
-        t.type = t.value.upper()  # Cambia el tipo de token si es una palabra clave
-    else:
-        print(f"Token encontrado: {t.value} como NOMBRE")  # Imprimir el token encontrado
+        #print(f'Token encontrado: {t.value} como {t.value.upper()}')  
+        t.type = t.value.upper()  
+    #else:
+    #    print(f'Token encontrado: {t.value} como NOMBRE')  
     return t
 t_STRING = r'\'[^\']*\''
 t_NUMERO = r'\d+'
 
 t_SUMA = r'\+'
 t_RESTA = r'-'
-t_MULTIPLICACION = r'\*'
 t_DIVISION = r'/'
 t_IGUAL = r'='
 t_MAYOR_QUE = r'>'
@@ -102,7 +100,7 @@ lexer = lex.lex()
 
 precedence = (
     ('left','SUMA','RESTA'),
-    ('left','MULTIPLICACION','DIVISION'),
+    ('left','DIVISION'),
     #('right','UMENOS'),
     ('left', 'Y', 'O'), 
 )
@@ -118,13 +116,48 @@ def p_statement(t):
 
 #Reglas para SELECT
 def p_select_statement(t):
-    'select_statement : TRAEME columns DE_LA_TABLA NOMBRE condition_opt'
-    t[0] = f"SELECT {t[2]} FROM {t[4]} {t[5]}"
+    '''select_statement : TRAEME columns DE_LA_TABLA NOMBRE join_clause condition_opt PUNTO_COMA
+                        | TRAEME columns DE_LA_TABLA NOMBRE join_clause condition_opt group_clause PUNTO_COMA'''
+    # Construye la consulta SQL con los elementos que no sean None
+    sql_query = f"SELECT {t[2]} FROM {t[4]}"
+    if t[5]: sql_query += f" {t[5]}"
+    if t[6]: sql_query += f" {t[6]}"
+    if len(t) > 8 and t[7]: sql_query += f" {t[7]}"
+    t[0] = sql_query
+
+# Nueva regla para manejar la cláusula JOIN
+def p_join_clause(t):
+    '''join_clause : MEZCLANDO NOMBRE EN join_condition
+                   | empty'''
+    t[0] = f'JOIN {t[2]} ON {t[4]}' if len(t) > 2 else ''
+
+def p_group_clause(t):
+    '''group_clause : AGRUPANDO_POR NOMBRE
+                    | AGRUPANDO_POR NOMBRE WHERE_DEL_GROUP_BY condition_count
+                    | empty'''
+    if len(t) == 5:  
+        t[0] = f'GROUP BY {t[2]} HAVING {t[4]}'
+    else:
+        t[0] = f'GROUP BY {t[2]}'
+
+def p_condition_count(t):
+    '''condition_count : COUNT PARENIZQ TODO PARENDER comparison value
+                       | empty'''
+    t[0] = f'COUNT{t[2]}{t[3]}{t[4]} {t[5]} {t[6]}'
+
+def p_join_condition(t):
+    '''join_condition : NOMBRE IGUAL NOMBRE
+                      | NOMBRE PUNTO NOMBRE IGUAL NOMBRE PUNTO NOMBRE
+                      | NOMBRE comparison value'''
+    if len(t) == 4:  
+        t[0] = f'{t[1]} = {t[3]}' 
+    elif len(t) == 8:
+        t[0] = f'{t[1]}{t[2]}{t[3]} {t[4]} {t[5]}{t[6]}{t[7]}'
 
 def p_condition_opt(t):
-    '''condition_opt : condition
+    '''condition_opt : condition_s
                      | empty'''
-    t[0] = t[1] if t[1] else ''
+    t[0] = t[1]  
 
 def p_empty(t):
     'empty :'
@@ -132,11 +165,16 @@ def p_empty(t):
 
 def p_columns(t):
     '''columns : TODO
-               | columns_list'''
+               | columns_list
+               | counting'''
     if t[1] == 'TODO':
-        t[0] = "*"
+        t[0] = '*'
     else:
-        t[0] = t[1]  
+        t[0] = t[1] 
+    
+def p_counting(t):
+    'counting : CONTANDO PARENIZQ TODO PARENDER'
+    t[0] = 'COUNT(*)'
 
 def p_columns_list(t):
     '''columns_list : NOMBRE
@@ -144,15 +182,19 @@ def p_columns_list(t):
     if len(t) == 2:
         t[0] = t[1]
     else:
-        t[0] = f"{t[1]}, {t[3]}"
+        t[0] = f"{t[1]} {t[2]} {t[3]}"
 
 def p_columns_distinct(t):
     'columns : LOS_DISTINTOS NOMBRE'
     t[0] = f"DISTINCT {t[2]}"
 
-def p_condition_where(t):
-    'condition : DONDE NOMBRE comparison value PUNTO_COMA'
-    t[0] = f"WHERE {t[2]} {t[3]} {t[4]}"
+def p_condition_where_s(t):
+    '''condition_s : DONDE NOMBRE comparison value
+                   | DONDE NOMBRE PUNTO NOMBRE comparison value'''
+    if len(t) == 5:
+        t[0] = f'WHERE {t[2]} {t[3]} {t[4]}'
+    elif len(t) == 7: 
+        t[0] = f'WHERE {t[2]}.{t[4]} {t[5]} {t[6]}'
 
 def p_value(t):
     '''value : NUMERO
@@ -170,15 +212,7 @@ def p_comparison_gt(t):
 #Reglas para INSERT
 def p_insert_statement(t):
     'insert_statement : METE_EN NOMBRE PARENIZQ columns_list PARENDER LOS_VALORES PARENIZQ values_list PARENDER PUNTO_COMA'
-    t[0] = f"INSERT INTO {t[2]} ({t[4]}) VALUES ({t[8]})"
-
-def p_columns_list(t):
-    '''columns_list : NOMBRE
-                    | columns_list COMA NOMBRE'''
-    if len(t) == 2:
-        t[0] = t[1]
-    else:
-        t[0] = f"{t[1]}, {t[3]}"
+    t[0] = f'INSERT INTO {t[2]} ({t[4]}) VALUES ({t[8]}){t[10]}'
 
 def p_values_list(t):
     '''values_list : value
@@ -190,57 +224,86 @@ def p_values_list(t):
 
 #Reglas para UPDATE
 def p_update_statement(t):
-    'update_statement : ACTUALIZA NOMBRE SETEA assignments DONDE condition PUNTO_COMA'
-    t[0] = f"UPDATE {t[2]} SET {t[4]} WHERE {t[6]}"
+    'update_statement : ACTUALIZA NOMBRE SETEA assignments DONDE condition_where PUNTO_COMA'
+    t[0] = f'UPDATE {t[2]} SET {t[4]} WHERE {t[6]}{t[7]}'
 
 def p_assignments(t):
-    'assignments : NOMBRE IGUAL value'
-    t[0] = f"{t[1]} = {t[3]}"
+    '''assignments : NOMBRE IGUAL value
+                   | assignments COMA NOMBRE IGUAL value'''
+    if len(t) == 4:
+        t[0] = f'{t[1]} = {t[3]}'
+    else:
+        t[0] = f"{t[1]}, {t[3]} = {t[5]}"
+
+def p_condition_where(t):
+    '''condition_where : NOMBRE comparison value
+                       | NOMBRE PUNTO NOMBRE comparison value
+                       | NOMBRE ENTRE value Y value'''
+    if len(t) == 4:  
+        t[0] = f'{t[1]} {t[2]} {t[3]}'
+    elif t[2] == 'ENTRE':  
+        t[0] = f'{t[1]} BETWEEN {t[3]} AND {t[5]}'
+    else:  
+        t[0] = f'{t[1]}{t[2]}{t[3]} {t[4]} {t[5]}'
 
 #Reglas para DELETE
 def p_delete_statement(t):
-    'delete_statement : BORRA_DE_LA NOMBRE DONDE condition PUNTO_COMA'
-    t[0] = f"DELETE FROM {t[2]} WHERE {t[4]}"
+    'delete_statement : BORRA_DE_LA_TABLA NOMBRE DONDE condition_where PUNTO_COMA'
+    t[0] = f'DELETE FROM {t[2]} WHERE {t[4]}{t[5]}'
 
 #Reglas para ALTER
 def p_alter_statement(t):
     'alter_statement : CAMBIA_LA_TABLA NOMBRE alter_actions PUNTO_COMA'
-    t[0] = f"ALTER TABLE {t[2]} {t[3]}"
+    t[0] = f"ALTER TABLE {t[2]} {t[3]}{t[4]}"
 
 def p_alter_actions(t):
     '''alter_actions : AGREGA_LA_COLUMNA NOMBRE VARCHAR PARENIZQ NUMERO PARENDER NO_NULO
                      | ELIMINA_LA_COLUMNA NOMBRE'''
     
     if t[1] == 'AGREGA_LA_COLUMNA':
-        t[0] = f"ADD COLUMN {t[2]} VARCHAR({t[4]}) NOT NULL"
+        t[0] = f'ADD COLUMN {t[2]} VARCHAR({t[5]}) NOT NULL'
     else:
         t[0] = f"DROP COLUMN {t[2]}"
 
 def p_error(t):
     print(f"Syntax error at '{t.value}' '{t.type} {type(t.type)}'")
 
-#Inicializamos parser
 parser = yacc.yacc()
 
+
+#Funciones personalizadas
+def es_consulta_sql_valida(sql):
+    """
+    Esta función toma una consulta en SQL y verifica si está con el formato válido.
+    """
+    try:
+        sqlparse.parse(sql)
+        return True
+    except:
+        return False
+    
 def traducir_usql_a_sql(consulta_usql):
     """
     Esta función toma una consulta en USQL y devuelve su equivalente en SQL.
     """
-    lexer.input(consulta_usql)
     try:
+        lexer.input(consulta_usql)
         while True:
             tok = lexer.token()
             if not tok:
                 break
-            print(tok)  # Añadido para depuración: imprime los tokens generados
         resultado = parser.parse(consulta_usql)
-        return resultado
+        if es_consulta_sql_valida(resultado):
+            return resultado
+        else:
+            raise ValueError("Consulta SQL no válida")
     except Exception as e:
-        print(f"Error al procesar la consulta USQL: {str(e)}")
+        print(f'Error al procesar la consulta USQL: {str(e)}')
         return None
 
 if __name__ == '__main__':
-    consulta_usql = "CAMBIA_LA_TABLA empleados AGREGA_LA_COLUMNA direccion VARCHAR(255) NO_NULO;"
+    consulta_usql = "TRAEME TODO DE_LA_TABLA usuarios DONDE edad > 18;"
     resultado = traducir_usql_a_sql(consulta_usql)
-    print(resultado)  # Debería devolver: SELECT * FROM usuarios WHERE edad > 18;
+    es_consulta_sql_valida(resultado)
+    print(resultado)  
  
